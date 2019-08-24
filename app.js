ad059726179dfa635d4c3d35246b28881b0d2d46
Steven Zamborsky <zam264@gmail.com>
2019-08-24 12:07:21 -0400

    Working example to get customer info by country

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const sqlite3 = require('sqlite3').verbose();
 
 let app = express();
 
-
-var o = {} // empty Object
-var key = 'Orientation Sensor';
-o[key] = []; // empty Array, which you can push() values into
-
-
-
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -20,14 +13,15 @@ app.listen(80, () => {
     console.log("Server running on port 80");
 });
 
-let sql = `SELECT FirstName firstName,
-                  LastName lastName,
-                  Email email
-            FROM customers
-            WHERE Country = ?
-            ORDER BY FirstName`;
+let getCustomersByCountry = (country, callback) => {
 
-let getCustomersByCountry = (callback) => {
+    let sql = `SELECT FirstName,
+        LastName,
+        Email
+        FROM customers
+        WHERE Country = ?
+        ORDER BY FirstName`;
+
     // open the database
     let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READONLY, (err) => {
         if (err) {
@@ -36,62 +30,34 @@ let getCustomersByCountry = (callback) => {
         console.log('Connected to the chinook database.');
     });
 
-    db.serialize( () => {
-        db.all(`SELECT * FROM customers LIMIT 5`, (err, allRows) => {
-            if(err != null){
-                console.log(err);
-                callback(err);
-            }
-            
-            // console.log(util.inspect(allRows));
-            // console.log(allRows);
-            console.log("in getCUstomersByContry");
-            
+    db.serialize(() => {
+        db.all(sql, country, (err, allRows) => {
             callback(allRows);
-            // callback("done");
             db.close();
         });
     });
 }
 
-// let greeting = (name) => {
-//     console.log('in call back ' + name);
-//     return "yup"
-//   }
-
-app.get("/url", async (req, res, next) => {
-    // res.json(getCustomersByCountry(greeting))
-    
-    getCustomersByCountry( (allRows) => {
+app.get("/url/:country", async (req, res, next) => {
+    getCustomersByCountry(req.params.country, (allRows) => {
         console.log("in callback");
-        console.log(allRows);
-        res.json("[yup]")
+
+        var o = {} // empty Object
+        var key = 'Customer Information';
+        o[key] = []; // empty Array, which you can push() values into
+
+        allRows.forEach(element => {
+
+            var data = {
+                firstName: element.FirstName,
+                lastName: element.LastName,
+                email: element.Email
+            }
+            console.log(data)
+            o[key].push(data)
+        });
+        jsonStr = JSON.stringify(o[key])
+        console.log(jsonStr)
+        res.send(o)
     })
-    // res.json("done")
 });
-// app.get("/url/:country", async (req, res, next) => {
-    
-// });
-
-
-
-// db.each(sql, req.params.country,  (err, row) => {
-//     if (err) {
-//         console.error(err.message);
-//     }
-//     var data = {
-//         firstName: row.firstName,
-//         lastName: row.lastName,
-//         email: row.email
-//     }
-//     o[key].push(data)
-// });
-// await res.json(o)
-
-
-// db.close((err) => {
-//   if (err) {
-//     console.error(err.message);
-//   }
-//   console.log('Close the database connection.');
-// }); 
--- main.js
+++ main.js
@@ -3,13 +3,37 @@ window.onload = () => {
 }
 
 let runQuery = async () => {
-    let response = await fetch('http://localhost:3000/url/USA', {
-        method: 'POST',
+    let value = document.querySelector("#queryText").value;
+
+    let response = await fetch(`http://192.168.86.56/url/${value}`, {
+        method: 'GET',
+        mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
-          }
+        }
     });
     let myJson = await response.json();
-
     console.log(myJson);
+    // parseJSON = JSON.parse()
+    strJson = JSON.stringify(myJson)
+    // document.querySelector("#customerInfo").innerHTML = strJson;
+
+    let data = myJson['Customer Information']
+
+    var html = '<table class="table">';
+    html += '<tr>';
+    var flag = 0;
+    $.each(data[0], (index, value) => {
+        html += '<th>' + index + '</th>';
+    });
+    html += '</tr>';
+    $.each(data, (index, value) => {
+        html += '<tr>';
+        $.each(value, (index2, value2) => {
+            html += '<td>' + value2 + '</td>';
+        });
+        html += '<tr>';
+    });
+    html += '</table>';
+    $('#customerInfo').html(html);
 }
