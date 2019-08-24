74cdafeb3e39aa0dbe0b0c2ff683089ee02b598b
Steven Zamborsky <zam264@gmail.com>
2019-08-24 00:38:54 -0400

    cors update

--- app.js
+++ app.js
@@ -16,8 +16,14 @@ let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READ, (err) => {
     console.log('Connected to the chinook database.');
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(80, () => {
+    console.log("Server running on port 80");
 });
 
 
@@ -29,18 +35,19 @@ let sql = `SELECT FirstName firstName,
             ORDER BY FirstName`;
 
 app.get("/url/:country", (req, res, next) => {
-    db.each(sql, req.params.country, (err, row) => {
-            if (err) {
-                console.error(err.message);
-            }
-            var data = {
-                firstName: row.firstName,
-                lastName: row.lastName,
-                email: row.email
-            }
-            o[key].push(data)
-        });
-    res.json(o)
+    // db.each(sql, req.params.country, (err, row) => {
+    //         if (err) {
+    //             console.error(err.message);
+    //         }
+    //         var data = {
+    //             firstName: row.firstName,
+    //             lastName: row.lastName,
+    //             email: row.email
+    //         }
+    //         o[key].push(data)
+    //     });
+    // res.json(o)
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"])
 });
 
 
